102a43b80ce7ca9776f03fcec6a6b49a6fd7fae5
Norwal Neto <100009853+norwalneto@users.noreply.github.com>
2025-05-01 18:21:26 +0000

    CRUD inicial

--- src/main/java/com/ecommerce/service/UserService.java
+++ src/main/java/com/ecommerce/service/UserService.java
@@ -1,8 +1,14 @@
 package com.ecommerce.service;
 
+import java.util.List;
+import java.util.UUID;
+
 import com.ecommerce.domain.dto.UserDTO;
+import com.ecommerce.domain.model.User;
 
 public interface UserService {
-    UserDTO register (UserDTO userDTO);
-    UserDTO login (UserDTO userDTO);
+    User listarPorId (UUID id);
+    List<User> listarTodos (User user);
+    User register (User user);
+    //UserDTO login (UserDTO userDTO);
 }
--- src/main/java/com/ecommerce/service/impl/UserServiceImpl.java
+++ src/main/java/com/ecommerce/service/impl/UserServiceImpl.java
@@ -1,23 +1,44 @@
 package com.ecommerce.service.impl;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
 import com.ecommerce.domain.dto.UserDTO;
+import com.ecommerce.domain.model.User;
+import com.ecommerce.domain.repository.UserRepository;
 import com.ecommerce.service.UserService;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserServiceImpl implements UserService {
 
-    @Override
-    public UserDTO register (UserDTO userDTO){
-        return List<E>.of();
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
     @Override
-    public UserDTO login (UserDTO userDTO){
-        return List<E>.of();
+    public User register (User user){
+        return userRepository.save(user);
+    }
+
+    //@Override
+    //public UserDTO login (UserDTO userDTO){
+        //return ;
+    //}
+
+    @Override
+    public List<User> listarTodos(User user) {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User listarPorId(UUID id) {
+        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuário não encontrado com id: " + id));
     }
 
 }
