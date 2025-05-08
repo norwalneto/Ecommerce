a16097da235163907b9a62aadf4ffa4f848a00fa
norwalneto <norwalgomess@gmail.com>
2025-05-08 00:13:11 -0300

    implmentação do swagger para documentação

--- src/main/java/com/ecommerce/EcommecerApplication.java
+++ src/main/java/com/ecommerce/EcommecerApplication.java
@@ -11,6 +11,10 @@ import com.ecommerce.config.OpenApiServerProperties;
 public class EcommecerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Iniciando aplicação...");
+        System.out.println("Variáveis:");
+        System.out.println("PORT: " + System.getenv("PORT"));
+        System.out.println("DB: " + System.getenv("MYSQLHOST"));
 		SpringApplication.run(EcommecerApplication.class, args);
 	}
 
