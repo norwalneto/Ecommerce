38490992b6edbbc71fcb188339c8ad91e74aabbf
norwalneto <norwalgomess@gmail.com>
2025-05-08 00:24:27 -0300

    implmentação do swagger para documentação

--- src/main/java/com/ecommerce/contoller/HealthController.java
+++ src/main/java/com/ecommerce/contoller/HealthController.java
@@ -1,5 +1,13 @@
 package com.ecommerce.contoller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
 
 }
